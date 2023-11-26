52d5b92a6f3076e75c7fed0b91a99a636ea32779
balandzin <balandzin2602@gmail.com>
2023-11-26 15:04:11 +0300

    add alert for user name and password

--- HW_5/ViewController.swift
+++ HW_5/ViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    @IBAction func forgotUserNameButton() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is User 😉" )
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is Password 😉")
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
