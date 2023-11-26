e8e7da026b616ed955e1429143a025fc59ae3a5c
balandzin <balandzin2602@gmail.com>
2023-11-26 09:14:54 +0300

    Init commit

---
+++ HW_5/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HW_5
+//
+//  Created by Антон Баландин on 25.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
